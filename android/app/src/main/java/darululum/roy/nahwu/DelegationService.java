9755f7d4a9a0c993923f86be2aca50df83b528a9
farhanroy <farhanroy120@gmail.com>
2021-02-26 09:18:38 +0700

    adding android TWA

---
+++ android/app/src/main/java/darululum/roy/nahwu/DelegationService.java
@@ -0,0 +1,14 @@
+package darululum.roy.nahwu;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
